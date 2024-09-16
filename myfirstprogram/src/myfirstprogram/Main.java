fa32ba529efad36cc8d78b99d558301aeedd9e52
mdsahil417 <mdsahil72940161@gmail.com>
2024-09-16 16:36:49 +0530

    add

---
+++ myfirstprogram/src/myfirstprogram/Main.java
@@ -0,0 +1,38 @@
+package myfirstprogram;
+
+//import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+String[][] cars = new String[3][3];
+cars[0][0] ="camera";
+cars[0][1] ="tesla";
+cars[0][2] ="sahil";
+cars[1][0] = "samir";
+cars[1][1]=" aftab";
+cars[1][2]="navinji";
+cars[2][0]="sonu";
+cars[2][1]="raja";
+cars[2][2]="sajiya";
+for(int i=0; i<cars.length; i++) {
+	System.out.println();
+for(int j=0; j<cars[i].length; j++) {
+	System.out.print(cars[i][j]+ " ");
+}
+	
+
+}}	
+	
+
+
+
+	}
+
+
+
+
+
+
